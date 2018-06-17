e43e50f980411c1ae5519e5bdda5e065981ea1e2
Steve Lyall <stevenlyall@gmail.com>
2018-06-17 13:49:48 -0700

    layout with map and title bar

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {Main} from '../main/Main';
+
+export class App extends Component {
+
+  render() {
+    return (
+      <Main></Main>
+    );
+  }
+}
---
+++ src/main/Main.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './Main.css';
+import MainMenuBar from './mainmenubar/MainManuBar';
+import Map from './map/Map';
+
+
+export class Main extends Component {
+
+    render() {
+        return (
+            <div className={'mainContainer'}>
+                <MainMenuBar></MainMenuBar>
+                <div className={'mapContainer'}>
+                    <Map></Map>
+                </div>
+            </div>
+        )
+    }
+
+
+}
